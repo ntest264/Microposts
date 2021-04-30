31294b00ce5c7ae14ad00762f00feab25e7ba156
n.test <testn960@gmail.com>
2021-04-30 07:10:34 +0000

    init

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    
+     protected $proxies = '*'; // 全プロキシを信用
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
