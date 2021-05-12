5c0ee5ff0f097b681f2abb52ad606da04702f051
n.test <testn960@gmail.com>
2021-05-12 04:57:04 +0000

    change user.unfavorite in favorite button to favorites.favorite

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,12 +1,12 @@
 @if (Auth::id() != $user->id)
     @if (Auth::user()->is_favoriting($user->id))
         {{-- 非追加ボタンのフォーム --}}
-        {!! Form::open(['route' => ['user.unfavorite', $user->id], 'method' => 'delete']) !!}
+        {!! Form::open(['route' => ['favorites.unfavorite', $user->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
         {!! Form::close() !!}
     @else
         {{-- お気に入り追加ボタンのフォーム --}}
-        {!! Form::open(['route' => ['user.favorite', $user->id]]) !!}
+        {!! Form::open(['route' => ['favorites.favorite', $user->id]]) !!}
             {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
         {!! Form::close() !!}
     @endif
