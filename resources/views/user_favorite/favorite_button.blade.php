40012b9b21406757d74f67a8bda5ef2609a1d993
n.test <testn960@gmail.com>
2021-05-13 04:50:53 +0000

    chang the size of favorite button to the right size

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -2,12 +2,12 @@
     @if (Auth::user()->is_favoriting($micropost->id))
         {{-- 非追加ボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger"]) !!}
         {!! Form::close() !!}
     @else
         {{-- お気に入り追加ボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-success btn-block"]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-success"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
