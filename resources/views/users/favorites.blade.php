0fe5e19fbfac670561dfef6cf423446fce3fe48a
n.test <testn960@gmail.com>
2021-05-13 06:47:08 +0000

    change the code of line13 in favorite,blade but have an error

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-@extends('layouts.app')
-
 @section('content')
     <div class="row">
         <aside class="col-sm-4">
@@ -12,41 +10,7 @@
             {{-- タブ --}}
             @include('users.navtabs')
             {{-- 投稿ー一覧 --}}
-          @if (count($microposts ?? '') > 0)
-    <ul class="list-unstyled">
-        @foreach ($microposts ?? '' ?? '' as $micropost)
-            <li class="media mb-3">
-                {{-- 投稿の所有者のメールアドレスをもとにGravatarを取得して表示 --}}
-                <img class="mr-2 rounded" src="{{ Gravatar::get($micropost->user->email, ['size' => 50]) }}" alt="">
-                <div class="media-body">
-                    <div>
-                        {{-- 投稿の所有者のユーザ詳細ページへのリンク --}}
-                        {!! link_to_route('users.show', $micropost->user->name, ['user' => $micropost->user->id]) !!}
-                        <span class="text-muted">posted at {{ $micropost->created_at }}</span>
-                    </div>
-                    <div>
-                        {{-- 投稿内容 --}}
-                        <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
-                    </div>
-                    <div>
-                        @if (Auth::id() == $micropost->user_id)
-                            {{-- 投稿削除ボタンのフォーム --}}
-                            {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
-                                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
-                            {!! Form::close() !!}
-                        @endif
-                    </div>
-                    <div>
-                        {{-- お気に入り追加／非追加ボタン --}}
-                        @include('user_favorite.favorite_button')
-                    </div>
-                </div>
-            </li>
-        @endforeach
-    </ul>
-    {{-- ページネーションのリンク --}}
-    {{ $microposts ?? '' ?? ''->links() }}
-@endif
+            @include('microposts.microposts')
         </div>
     </div>
 @endsection
