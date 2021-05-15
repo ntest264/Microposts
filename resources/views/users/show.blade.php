22d33f6db614719f2b5ab4854490b8372568742f
n.test <testn960@gmail.com>
2021-05-15 08:06:48 +0000

    add the contents of microposts.microposts in welcome.blade and successfully show user name

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,7 +14,40 @@
                 @include('microposts.form')
             @endif
             {{-- 投稿一覧 --}}
-            @include('microposts.microposts')
+           {{-- @include('microposts.microposts')--}}
+           @if (count($microposts) > 0)
+        <ul class="list-unstyled">
+           @foreach ($microposts as $micropost)
+            <li class="media mb-3">
+                {{-- 投稿の所有者のメールアドレスをもとにGravatarを取得して表示 --}}
+                <img class="mr-2 rounded" src="{{ Gravatar::get($micropost->user->email, ['size' => 50]) }}" alt="">
+                <div class="media-body">
+                    <div>
+                        {{-- 投稿の所有者のユーザ詳細ページへのリンク --}}
+                        {!! link_to_route('users.show', $micropost->user->name, ['user' => $micropost->user->id]) !!}
+                        <span class="text-muted">posted at {{ $micropost->created_at }}</span>
+                    </div>
+                    <div>
+                        {{-- 投稿内容 --}}
+                        <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                        {{-- お気に入り追加／非追加ボタン --}}
+                        @include('user_favorite.favorite_button')
+                    </div>
+                    <div>
+                        @if (Auth::id() == $micropost->user_id)
+                            {{-- 投稿削除ボタンのフォーム --}}
+                            {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                            {!! Form::close() !!}
+                        @endif
+                    </div>
+                </div>
+            </li>
+            @endforeach
+        </ul>
+           {{-- ページネーションのリンク --}}
+           {{ $microposts->links() }}
+            @endif
         </div>
     </div>
 @endsection
